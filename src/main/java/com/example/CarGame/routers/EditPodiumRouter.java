149f8a19674ddd95d9fabc1e343240b4ac1ec97c
saracarohdm <saracarohdm@gmail.com>
2021-08-13 23:25:42 -0500

    se realizo el useCase y router de podium edit

---
+++ src/main/java/com/example/CarGame/routers/EditPodiumRouter.java
@@ -0,0 +1,28 @@
+package com.example.CarGame.routers;
+
+import com.example.CarGame.dtos.PodiumDTO;
+import com.example.CarGame.useCase.podium.CreatePodiumUseCase;
+import com.example.CarGame.useCase.podium.EditPodiumUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class EditPodiumRouter {
+    @Bean
+    public RouterFunction<ServerResponse> editPodium(EditPodiumUseCase editPodiumUseCase){
+        return route(PUT("/editPodium").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(PodiumDTO.class).flatMap(podiumDTO -> editPodiumUseCase.apply(podiumDTO)
+                        .flatMap(result -> ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(result)
+                        )
+                )
+        );
+    }
+}
--- src/main/java/com/example/CarGame/useCase/podium/EditPodiumUseCase.java
+++ src/main/java/com/example/CarGame/useCase/podium/EditPodiumUseCase.java
@@ -1,4 +1,27 @@
 package com.example.CarGame.useCase.podium;
 
+import com.example.CarGame.dtos.PodiumDTO;
+import com.example.CarGame.repository.RepositoryPodium;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+@Service
+@Validated
+
 public class EditPodiumUseCase {
+
+    private final RepositoryPodium repositoryPodium;
+
+    @Autowired
+    public EditPodiumUseCase(RepositoryPodium repositoryPodium) {
+        this.repositoryPodium = repositoryPodium;
+
+    }
+
+    public Mono<PodiumDTO> apply(PodiumDTO podiumDTO) {
+        return repositoryPodium.save(podiumDTO)
+                .thenReturn(podiumDTO);
+    }
 }

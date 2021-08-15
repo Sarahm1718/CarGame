5e1f456f9ce09cf68cb16f0a51cae386031d8dad
saracarohdm <saracarohdm@gmail.com>
2021-08-15 18:07:43 -0500

    se creo el useCase de finbyGameIdPodium

--- src/main/java/com/example/CarGame/repository/RepositoryPodium.java
+++ src/main/java/com/example/CarGame/repository/RepositoryPodium.java
@@ -4,8 +4,10 @@ import com.example.CarGame.domain.game.Podium;
 import com.example.CarGame.dtos.PodiumDTO;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 
 public interface RepositoryPodium extends ReactiveMongoRepository<Podium, String> {
+    Flux<Podium> findByGameId(String gameId);
 }
--- src/main/java/com/example/CarGame/useCase/podium/FindByGameIdPodiumUseCase.java
+++ src/main/java/com/example/CarGame/useCase/podium/FindByGameIdPodiumUseCase.java
@@ -1,6 +1,7 @@
-/*package com.example.CarGame.useCase.podium;
+package com.example.CarGame.useCase.podium;
 
 import com.example.CarGame.dtos.PodiumDTO;
+import com.example.CarGame.mapper.MapperPodium;
 import com.example.CarGame.repository.RepositoryPodium;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,13 +12,16 @@ import reactor.core.publisher.Flux;
 @Validated
 public class FindByGameIdPodium {
     private final RepositoryPodium repositoryPodium;
+    private final MapperPodium mapperPodium;
 
 @Autowired
-    public FindByGameIdPodium(RepositoryPodium repositoryPodium) {
+    public FindByGameIdPodium(RepositoryPodium repositoryPodium, MapperPodium mapperPodium) {
         this.repositoryPodium = repositoryPodium;
-    }
+    this.mapperPodium = mapperPodium;
+}
 
     public Flux<PodiumDTO>getPodiumByGameId(String id){
-    return repositoryPodium.findAll(id);
+    return repositoryPodium.findByGameId(id)
+            .flatMap(podium -> Flux.just(mapperPodium.mapperToDTO().apply(podium)));
     }
-}*/
+}

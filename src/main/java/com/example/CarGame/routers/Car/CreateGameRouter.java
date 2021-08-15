286297f1d896d316decfba6369555dfdfab67fd9
saracarohdm <saracarohdm@gmail.com>
2021-08-15 13:14:31 -0500

    se creo el use case de create game

---
+++ src/main/java/com/example/CarGame/routers/Car/CreateGameRouter.java
@@ -0,0 +1,4 @@
+package com.example.CarGame.routers.Car;
+
+public class CreateGameRouter {
+}
--- src/main/java/com/example/CarGame/useCase/Game/CreateGameUseCase.java
+++ src/main/java/com/example/CarGame/useCase/Game/CreateGameUseCase.java
@@ -1,4 +1,29 @@
 package com.example.CarGame.useCase.Game;
 
+import com.example.CarGame.domain.game.Game;
+import com.example.CarGame.dtos.GameDTO;
+import com.example.CarGame.mapper.MapperGame;
+import com.example.CarGame.repository.RepositoryGame;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+@Service
+@Validated
 public class CreateGameUseCase {
+    private final RepositoryGame repositoryGame;
+    private final MapperGame mapperGame;
+
+    @Autowired
+    public CreateGameUseCase(RepositoryGame repositoryGame, MapperGame mapperGame) {
+        this.repositoryGame = repositoryGame;
+        this.mapperGame = mapperGame;
+    }
+
+    public Mono<GameDTO> GameCreate(GameDTO gameDTO){
+        return repositoryGame.save(mapperGame.mapperToGame(gameDTO.getGameId())
+        .apply(gameDTO))
+                .map(mapperGame.mapperToGameDTO());
+    }
 }
